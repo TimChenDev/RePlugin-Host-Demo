031a8444e1530278396d3a63ec696ff8e3bf6918
TimChenDev <devtimchentw@gmail.com>
2020-07-22 15:58:17 +0800

    feat: add RePlugin host settings
    1. use RePlugin version v2.2.4
    2. use gradle plugin version v3.1.0
    3. replace support library with androidx
    4. apply plugin: 'replugin-host-gradle'
    5. add repluginHostConfig

--- app/src/main/java/com/devtimchentw/replugin_host_demo/MainActivity.kt
+++ app/src/main/java/com/devtimchentw/replugin_host_demo/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.devtimchentw.replugin_host_demo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import com.qihoo360.replugin.RePlugin
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,10 +19,22 @@ class MainActivity : AppCompatActivity() {
 
         btn_go_to_plugin_one.setOnClickListener {
             Log.v(TAG, "btn_go_to_plugin_one clicked!")
+            RePlugin.startActivity(
+                this@MainActivity,
+                Intent(),
+                "app-debug", // 這裡使用 jar 的檔名或者 plugin 的 packageName 都可以
+                "com.devtimchentw.replugin_pluginone_demo.MainActivity"
+            )
         }
 
         btn_go_to_plugin_two.setOnClickListener {
             Log.v(TAG, "btn_go_to_plugin_two clicked!")
+            RePlugin.startActivity(
+                this@MainActivity,
+                Intent(),
+                "app-debug2", // 這裡使用 jar 的檔名或者 plugin 的 packageName 都可以
+                "com.devtimchentw.replugin_plugintwo_demo.MainActivity"
+            )
         }
     }
 }
---
+++ app/src/main/java/com/devtimchentw/replugin_host_demo/MyApplication.kt
@@ -0,0 +1,12 @@
+package com.devtimchentw.replugin_host_demo
+
+import com.qihoo360.replugin.RePluginApplication
+
+/**
+ *  author: Tim Chen
+ *  time  : 2020-07-22
+ *  desc  :
+ */
+class MyApplication : RePluginApplication() {
+
+}
