ab5373000997faf1357fdb4194e49e0ae325ff9e
TimChenDev <devtimchentw@gmail.com>
2020-07-22 14:40:47 +0800

    feat: draw activity_main, add two button for go to plugin

--- app/src/main/java/com/devtimchentw/replugin_host_demo/MainActivity.kt
+++ app/src/main/java/com/devtimchentw/replugin_host_demo/MainActivity.kt
@@ -2,11 +2,25 @@ package com.devtimchentw.replugin_host_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_go_to_plugin_one.setOnClickListener {
+            Log.v(TAG, "btn_go_to_plugin_one clicked!")
+        }
+
+        btn_go_to_plugin_two.setOnClickListener {
+            Log.v(TAG, "btn_go_to_plugin_two clicked!")
+        }
     }
 }
